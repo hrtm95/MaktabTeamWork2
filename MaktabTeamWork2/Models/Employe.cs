63f04f5f6588a1546f50632d658a1fdf69b29790
shayanmdh86 <shayanmdhy0@gmail.com>
2023-05-19 11:39:33 +0330

    Add models in git

---
+++ MaktabTeamWork2/Models/Employe.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaktabTeamWork2.Models
+{
+    public class Employe
+    {
+
+        string id { get; set; }
+        string FName { get; set; }
+
+        string LName { get; set; }
+
+        string nationalCode { get; set; }
+
+        string mobile { get; set; }
+
+        string gender { get; set; }
+
+        string Email { get; set; }
+    }
+}
---
+++ MaktabTeamWork2/Models/Salary.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaktabTeamWork2.Models
+{
+    public class Salary
+    {
+
+        string Id { get; set; }
+
+        double Amont { get; set; }
+
+        double tax { get; set; }
+
+        double oderBenefit { get; set; }
+
+        string EmployeeId { get; set; }
+
+        TimeSpan year { get; set; }
+
+        TimeSpan month { get; set; }
+    }
+}
